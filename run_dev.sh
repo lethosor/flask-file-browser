ec432bbd10c44768f4a9b8c555a29e7ae6c19c89
lethosor <lethosor@gmail.com>
2018-08-03 13:19:46 -0400

    run scripts: activate venv

--- run_dev.sh
+++ run_dev.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd $(dirname "$0")
+. bin/activate
 export FLASK_DEBUG=true
 export CONFIG=config.DevConfig
 flask run
--- run_prod.sh
+++ run_prod.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd $(dirname "$0")
+. bin/activate
 export FLASK_DEBUG=false
 export CONFIG=config.ProdConfig
 gunicorn -b unix:./.sock wsgi
