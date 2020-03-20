70c356bee02af09d83463445dc97b462c192ebe3
lethosor <lethosor@gmail.com>
2020-03-19 22:43:32 -0400

    Fix/update site name configuration

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
 import os
 
-SITE_NAME = 'Files'
+SITE_NAME = os.getenv('SITE_NAME', 'Files')
 FILE_PATH = os.path.join(os.getcwd(), 'files')
-
-try:
-    from local_config import *
-except ImportError:
-    pass
